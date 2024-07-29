66873ecd86fa23a10226dbf588c71cf0fb034f56
javan7875 <javanh1996@gmail.com>
2024-07-29 17:34:13 +0330

    Complete ProductAggregate Task

---
+++ Shop.Domain/ProductAgg/ProductImage.cs
@@ -0,0 +1,19 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.ProductAgg
+{
+    public class ProductImage :BaseEntity
+    {
+        public ProductImage(string imageName, int sequance)
+        {
+            NullOrEmptyDomainDataException.CheckString(imageName, nameof(imageName));
+            ImageName = imageName;
+            Sequance = sequance;
+        }
+
+        public long ProductId { get; internal set; }
+        public string ImageName { get; private set; }
+        public int Sequance { get; private set; }
+    } 
+}
